bfe4b472049ab97fedeca0b3b3eacffbd60353a1
Kashif Shahzad <142479417+KashifShahzad999@users.noreply.github.com>
2025-08-02 16:35:42 +0500

    change name

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-900">
       <div className="w-72 h-72 bg-black rounded-full shadow-2xl flex flex-col items-center justify-center text-white border-8 border-gray-800">
-        <h1 className="text-2xl font-bold mb-2 tracking-wide">Smart Watch by ks</h1>
+        <h1 className="text-2xl font-bold mb-2 tracking-wide">Smart Watch </h1>
         <p className="text-center text-lg">{dateTime}</p>
       </div>
     </div>
